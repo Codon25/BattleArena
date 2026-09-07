f118aee95b861f7d2125d6df8d6801b8916c1060
codon25 <jomarcods25@gmail.com>
2026-09-07 13:52:27 +0800

    Add battle loop and special attacks to Warrior class

--- BattleArena/Program.cs
+++ BattleArena/Program.cs
@@ -12,22 +12,25 @@ namespace BattleArena
     {
         static void Main(string[] args)
         {
-            Warrior Raymond = new Warrior("Raymond", 100, 30);
-            Warrior Kirk = new Warrior("Kirk", 200, 15);
-            Warrior Cods = new Warrior("Cods", 150, 30);
+            int round = 1;
+            Warrior Raymond = new Warrior("Raymond", 100, 30, "Dinuraan");
+            Warrior Kirk = new Warrior("Kirk", 200, 15, "Dinaganan");
+            Warrior Cods = new Warrior("Cods", 150, 30, "Fire Ball");
 
+            Raymond.DisplayStats();
+            Kirk.DisplayStats();
+            Cods.DisplayStats();
+             
+            while(Raymond.IsAlive && Kirk.IsAlive && Cods.IsAlive)
+            {
+                Console.WriteLine($"------Round {round}------");
+                Raymond.Attack(Kirk);
+                Kirk.Attack(Cods);
+                Cods.Attack(Raymond);
+                Console.WriteLine("----------------");
+                round++;
+            }
 
-
-
-            Console.WriteLine($"{Raymond.Name} has " +
-                $"{Raymond.Health} health and {Raymond.AttackPower} attack power.");
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine($"{Kirk.Name} has" +
-                $" {Kirk.Health} health and {Kirk.AttackPower} attack power.");
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine($"{Cods.Name} has" +
-                $" {Cods.Health} health and {Cods.AttackPower} attack power.");
-            Console.WriteLine("---------------------------------");
             Console.ReadKey();
         }
     } 
